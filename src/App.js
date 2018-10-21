41c9fd0b6a2e1d5c6785c3e2e6a8b0b91f38925e
Jason Cabrera <cabrerajason7@gmail.com>
2018-10-21 00:15:59 -0700

    map component react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Map from './components/Map.jsx';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Map app_id = "hM6QGhSurmnxc8i69ynP" app_code="GoO9Ti5ehS-nrEQQcm-KGQ" />
         </div>
       
     );
